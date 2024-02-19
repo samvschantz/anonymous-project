5eafbafd9b716310e70ae31acb943aa4401df8fa
Sam Schantz <samschantz@Sams-MacBook-Pro.local>
2024-02-19 14:47:21 -0800

    Initial commit of create react app typescript template and MUI components

---
+++ project/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => 
+(
+  <div className="App">
+    <header className="App-header">
+      <CatFactDisplay />
+    </header>
+  </div>
+);
+
+export default App;
---
+++ project/src/components/CatFactDisplay/CatFactDisplay.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const CatFactDisplay = () =>
+(
+    <div className="App">
+        <header className="App-header">
+        <CatFactDisplay />
+        </header>
+    </div>
+);
+
+export default CatFactDisplay;
